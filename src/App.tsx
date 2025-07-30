0d95bc02eea22a11524bd7d2e6ae1fd367b631c7
portora1 <nakajinn.1up@gmail.com>
2025-07-31 08:39:18 +0900

    お気に入り機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
 
 // APIから返ってくるデータの内使うものだけ定義
@@ -18,13 +18,18 @@ function App() {
   const [city, setCity] = useState('');
 
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  
+  const [favorites, setFavorites] = useState<string[]>(() => {
+    const saveFavorites = localStorage.getItem('weatherFavorites');
+    return saveFavorites ? JSON.parse(saveFavorites) : [];
+  });
 
   const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
-  const handleSearch = async() => {
-    if(!city) return;
+  const handleSearch = async(searchCity: string) => {
+    if(!searchCity) return;
 
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=ja`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${API_KEY}&units=metric&lang=ja`;
 
     try {
       const response = await fetch(URL);
@@ -38,8 +43,23 @@ function App() {
   };
   const handleFormSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    handleSearch();
+    handleSearch(city);
   };
+
+  const handleAddToFavorites = () => {
+    if (!weatherData) return;
+    if(!favorites.includes(weatherData.name)) {
+      setFavorites([...favorites, weatherData.name]);
+    }
+  };
+
+  const handleRemoveFavorites = (cityToRemove:string) => {
+    setFavorites(favorites.filter(city => city !== cityToRemove));
+  };
+
+  useEffect(() => {
+    localStorage.setItem('weatherFavorites', JSON.stringify(favorites));
+  }, [favorites]);
 
   return (
     <div className="App">
@@ -53,11 +73,29 @@ function App() {
       />
       <button type="submit">検索</button>
       </form>
+      <div className="favolites-list">
+        <h3>お気に入りリスト</h3>
+        {favorites.length > 0 ? (
+          <ul>
+            {favorites.map((favCity) => (
+              <li key={favCity}>
+                <span className="favorite-city" onClick={() => handleSearch(favCity)}>
+                  {favCity}
+                </span>
+                <button onClick={() => handleRemoveFavorites(favCity)}>削除</button>
+              </li>
+            ))}
+          </ul>
+            ) : (
+              <p>お気に入りの都市はありません。</p>
+            )}
+      </div>
     {weatherData ? (
     <div className="weather-result">
       <h2>都市名:{weatherData.name}</h2>
       <p>天気：{weatherData.weather[0].main}</p>
       <p>気温:{weatherData?.main.temp} ℃</p>
+      <button onClick={handleAddToFavorites}>お気に入りに追加</button>
         <img
           src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
           alt="weather icon"
