5b883f0cc2571352e494f6400432bdbf35a4a060
portora1 <nakajinn.1up@gmail.com>
2025-08-01 15:36:30 +0900

    お気に入り登録済み作成

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -4,9 +4,10 @@ import { type WeatherData } from '../App';
 type WeatherCardProps = {
   weatherData: WeatherData;
   onAddFavorite: () => void;
+  isFavorite: boolean;
 };
 
-export const WeatherCard = ({ weatherData, onAddFavorite }:
+export const WeatherCard = ({ weatherData, onAddFavorite, isFavorite }:
     WeatherCardProps) => {
     return (
         <div className="weather-card">
@@ -17,7 +18,11 @@ export const WeatherCard = ({ weatherData, onAddFavorite }:
               src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
         alt="weather icon"
       />
+      {isFavorite ? (
+        <p className="favorite-status">お気に入り登録済み</p>
+      ) : (
       <button onClick={onAddFavorite}>お気に入りに追加</button>
+          )}
         </div>
     );
 };
